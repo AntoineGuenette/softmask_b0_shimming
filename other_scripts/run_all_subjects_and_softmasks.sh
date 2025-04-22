15445d7c828a8aabf1e028857a8a5abd9d028124
AntoineGuenette <70295563+AntoineGuenette@users.noreply.github.com>
2025-04-21 21:10:17 -0400

    Add script to shim the segmentation and the binary mask only

--- other_scripts/run_all_subjects_and_softmasks.sh
+++ other_scripts/run_all_subjects_and_softmasks.sh
@@ -2,6 +2,8 @@
 
 echo -e "\n--- RUNNING ALL SOFTMASKS FOR SUBJECT 01 --- "
 
+echo -e "\n--- RUNNING SEGMENTATION AND BINARY MASK ---\n"
+../experiment_scripts/bin_seg_shim.sh /Users/antoineguenette/Desktop/démonstration_projet_III/Sujets/01 01 25 9 1
 echo -e "\n--- RUNNING 2 LEVELS SOFTMASK ---\n"
 ../experiment_scripts/softmask_shim.sh /Users/antoineguenette/Desktop/démonstration_projet_III/Sujets/01 01 25 9 2levels 1
 echo -e "\n--- RUNNING LINEAR SOFTMASK ---\n"
@@ -13,6 +15,8 @@ echo -e "\n--- RUNNING HYBRID SOFTMASK ---\n"
 
 echo -e "\n--- RUNNING ALL SOFTMASKS FOR SUBJECT 02 ---\n"
 
+echo -e "\n--- RUNNING SEGMENTATION AND BINARY MASK ---\n"
+../experiment_scripts/bin_seg_shim.sh /Users/antoineguenette/Desktop/démonstration_projet_III/Sujets/02 02 25 9 1
 echo -e "\n--- RUNNING 2 LEVELS SOFTMASK ---\n"
 ../experiment_scripts/softmask_shim.sh /Users/antoineguenette/Desktop/démonstration_projet_III/Sujets/02 02 25 9 2levels 1
 echo -e "\n--- RUNNING LINEAR SOFTMASK ---\n"
