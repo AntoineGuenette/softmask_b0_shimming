ca9f0a8cc5c4a763c08b63127ef840c33780953b
AntoineGuenette <70295563+AntoineGuenette@users.noreply.github.com>
2025-05-27 09:14:32 -0400

    Use only segmentation mask instead of each shimming mask

--- figure_scripts/violin_plot.py
+++ figure_scripts/violin_plot.py
@@ -2,35 +2,32 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import nibabel as nib
-from shimmingtoolbox.masking.mask_utils import resample_mask
+from nibabel.processing import resample_from_to
 import pandas as pd
 import os
 
 def load_subject_data(subject_paths, name):
     mask_img = nib.load(subject_paths["mask_path"])
     fm_imgs = [nib.load(fm_path) for fm_path in subject_paths["fm_paths"]]
+    fm_ref_img = fm_imgs[1]  # Use the second fieldmap (DynSHim_SCseg) as reference for resampling
     
     # resample mask to fm resolution
-    mask_imgs = [resample_mask(mask_img, fm_img) for fm_img in fm_imgs]
+    mask_img = resample_from_to(mask_img, fm_ref_img, order=0, mode='grid-constant', cval=0)
     
     # Replace 0 with NaN in mask
-    mask_imgs_data = [mask_img.get_fdata() for mask_img in mask_imgs]
-    mask_imgs_data = [np.where(mask == 0, np.nan, mask) for mask in mask_imgs_data]
+    mask_img_data = mask_img.get_fdata()
+    mask_img_data[mask_img_data == 0] = np.nan
     
     # apply binary mask to fm images
-    fm_imgs_data = []
-    for mask_img_data, fm_img in zip(mask_imgs_data, fm_imgs):
-        fm_data = fm_img.get_fdata()
-        fm_data = np.where(np.isnan(mask_img_data), np.nan, fm_data)
-        fm_imgs_data.append(fm_data)
+    fm_imgs_data = [fm_img.get_fdata() * mask_img_data for fm_img in fm_imgs]
     
-    return {'name': name, 'masks':mask_imgs_data, 'fms': fm_imgs_data}
+    return {'name': name, 'mask':mask_img_data, 'fms': fm_imgs_data}
 
-def compute_slice_wise_weighted_rmse(fm_data, weight_mask):
+def compute_slice_wise_weighted_rmse(fm_data, mask):
     rmses = []
     for slice_idx in range(fm_data.shape[-1]):
         fm_slice = fm_data[..., slice_idx]
-        weight_slice = np.sqrt(weight_mask[..., slice_idx])
+        weight_slice = np.sqrt(mask[..., slice_idx])
         # Ignore NaNs in both fm_slice and weight_slice
         valid = ~np.isnan(fm_slice) & ~np.isnan(weight_slice)
         if np.any(valid):
@@ -41,11 +38,10 @@ def compute_slice_wise_weighted_rmse(fm_data, weight_mask):
     return rmses
 
 def compute_rmse_subject(subject_data):
-    # create [] to store rmses if not already present
     if 'rmses' not in subject_data:
         subject_data['rmses'] = []
-    for fm_img, mask in zip (subject_data['fms'], subject_data['masks']):
-        rmses = compute_slice_wise_weighted_rmse(fm_img, mask)
+    for fm_img in subject_data['fms']:
+        rmses = compute_slice_wise_weighted_rmse(fm_img, subject_data['mask'])
         subject_data['rmses'].append(rmses)
     subject_data['rmses_mean'] = [np.nanmean(rmses) for rmses in subject_data['rmses']]
     subject_data['rmses_std'] = [np.nanstd(rmses) for rmses in subject_data['rmses']]
@@ -76,12 +72,14 @@ def make_df_from_subject_data(subject_data_list):
     
     return df
 
-def violin_plot_rmses_subjects(df):
+def violin_plot_rmses_subjects(df, output_path):
     # Create the violin plot with hue based on the subject
     plt.figure(figsize=(15, 8))
     sns.violinplot(x='Shim', y='RMSE', hue='Shim', data=df, cut=0)
     # Formatting
-    plt.xticks(ticks=[0, 1, 2, 3, 4, 5], labels=['Baseline\n(pas de shimming)', 'Binaire\nSegmentation', 'Binaire\nCylindrique', 'Pondéré\nDeux niveaux', 'Pondéré\nLinéaire', 'Pondéré\nGaussien'])
+    plt.xticks(ticks=[0, 1, 2, 3, 4, 5], 
+               labels=['Baseline\n(pas de shimming)', 'Binaire\nSegmentation', 'Binaire\nCylindrique',
+                       'Pondéré\nDeux niveaux', 'Pondéré\nLinéaire', 'Pondéré\nGaussien'])
     plt.xlabel("Masque utilisé pour le shimming")
     plt.ylabel('RMSE dans la moelle épinière')
     plt.title('Distribution tranche par tranche de la RMSE dans la moelle épinière')
@@ -97,22 +95,25 @@ def violin_plot_rmses_subjects(df):
     plt.grid(axis='y')
     
     # Save the figure
-    output_path = "/Users/antoineguenette/Desktop/Scolaire/NeuroPoly/Stage_E25/Experiences/2025.05.12-acdc_274/figures"
     output_file = os.path.join(output_path, "violin_plot.png")
     plt.savefig(output_file, dpi=300, bbox_inches='tight')
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-options = ['baseline', 'seg', 'bin', '2lvl', 'lin', 'gaus']
-subject_paths = {
-    "mask_path": os.path.join(script_dir, "../../2025.05.12-acdc_274/sub-acdc274/derivatives/masks/segmentation.nii.gz"),
-    "fm_paths": [os.path.join(script_dir, f"../../2025.05.12-acdc_274/fmap-acdc274/sub-acdc274_fmap_{option}.nii.gz") for option in options]
-}
+if __name__ == "__main__":
 
-subject_data = load_subject_data(subject_paths, 'acdc274')
-compute_rmse_subject(subject_data)
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    options = ['baseline', 'seg', 'bin', '2lvl', 'lin', 'gaus']
+    subject_paths = {
+        "mask_path": os.path.join(script_dir, "../../2025.05.12-acdc_274/sub-acdc274/derivatives/masks/segmentation.nii.gz"),
+        "fm_paths": [os.path.join(script_dir, f"../../2025.05.12-acdc_274/fmap-acdc274/sub-acdc274_fmap_{option}.nii.gz")
+                    for option in options]
+    }
 
-# create a DataFrame from the subject data
-df = make_df_from_subject_data([subject_data])
+    subject_data = load_subject_data(subject_paths, 'acdc274')
+    compute_rmse_subject(subject_data)
 
-# Plot violin plot with hue based on subject
-violin_plot_rmses_subjects(df)
+    # create a DataFrame from the subject data
+    df = make_df_from_subject_data([subject_data])
+
+    # Plot violin plot with hue based on subject
+    output_path = os.path.join(script_dir, "../../2025.05.12-acdc_274/figures")
+    violin_plot_rmses_subjects(df, output_path)
